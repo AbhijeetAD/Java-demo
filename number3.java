066814b5880d169f49bf47a3affb95d244ade80b
Abhijeet Pawar <pabhijeet552@gmail.com>
2024-07-30 15:07:32 +0530

    Commited

---
+++ number3.java
@@ -0,0 +1,13 @@
+ class MathOperations {
+
+    public static void main(String[] args) {
+        int number = 2345;
+        int addedNumber = number + 8;
+        int quotient = addedNumber / 3;
+        int modulus = quotient % 5;
+        int finalResult = modulus * 5;
+
+        
+        System.out.println("The final result is: " + finalResult);
+    }
+}
